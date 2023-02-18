11274d716ed11be9db4bfc22b6430c19fa9f22ba
bhaskar966 <deybhaskar999@gmail.com>
2023-02-18 22:30:39 +0530

    sweet : Update vendorsetup.sh
    *Track bliss Common tree
    *Track hardware_xiaomi from LineageOS

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,8 +14,10 @@ git clone https://github.com/ArrowOS/android_hardware_qcom_display -b arrow-13.0
 git clone https://github.com/LineageOS/android_packages_apps_Aperture.git -b lineage-20.0 packages/apps/Aperture
 
 # Common dt
-git clone https://github.com/bhaskar966/device_xiaomi_sm6150.git -b 13.0 device/xiaomi/sm6150-common
+git clone https://github.com/bhaskar966/device_xiaomi_sm6150_bliss.git -b 13.0 device/xiaomi/sm6150-common
 
+# hardware_xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 harware/xiaomi
 # Vendor
 git clone https://github.com/bhaskar966/vendor_xiaomi.git -b 13.0 vendor/xiaomi
 
